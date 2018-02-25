d2de1e0eae56dd8ab5a2ef53793080787257b140
pragjna <ps745@njit.edu>
2018-02-24 23:02:16 -0500

    final step

---
+++ resources/views/pages/viewmsgs.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">S.no</th>
+            <th scope="col">Contact Messages</th>
+            <th scope="col"></th>
+
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td><a href="http://localhost:8000/bootstrapcard">Sign up issue</td>
+            <td>
+                <div class="card at pl-0 pl-0 pl-0" align="center">
+                    <form>
+                        <div class="card">
+                            <div class="card-title"><h1></h1>
+                            </div>
+                            <div class="card-body">
+                                <button type="Delete" class="btn btn-primary mb-2">Delete</button>
+                    </form>
+                </div>
+                </div></td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td><a href="http://localhost:8000/bootstrapcard1">Can't access my account</td>
+            <td>
+                <div class="card at pl-0 pl-0 pl-0" align="center">
+                    <form>
+                        <div class="card">
+                            <div class="card-title"><h1></h1>
+                            </div>
+                            <div class="card-body">
+                                <button type="Delete" class="btn btn-primary mb-2">Delete</button>
+                    </form>
+                </div>
+                </div></td>
+        </tr>
+        <th scope="row">3</th>
+        <td><a href="http://localhost:8000/bootstrapcard2">Password reset issue</td>
+        <td>
+            <div class="card at pl-0 pl-0 pl-0" align="center">
+                <form>
+                    <div class="card">
+                        <div class="card-title"><h1></h1>
+                        </div>
+                        <div class="card-body">
+                            <button type="Delete" class="btn btn-primary mb-2">Delete</button>
+                </form>
+            </div>
+            </div></td>
+        </tr>
+        </tbody>
+    </table>
+@endsection
